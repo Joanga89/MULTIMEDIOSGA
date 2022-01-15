22c6193b12dcae2b22c03739a117ea14193088b4
Joanga89 <95677979+Joanga89@users.noreply.github.com>
2022-01-14 21:53:30 -0600

    Add files via upload

---
+++ andy.js
@@ -0,0 +1,6 @@
+materialIcons = '<link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">'
+buttonStyles = '<link href="https://codepen.io/andytran/pen/vLmRVp.css" rel="stylesheet">'
+
+
+
+document.body.innerHTML += materialIcons + buttonStyles + button
